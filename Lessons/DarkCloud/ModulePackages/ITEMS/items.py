0ff9f67257e7df3cbb7f9e4b2cf50373ba364e70
Moore Kitsune <daintygenesis@gmail.com>
2023-01-01 12:20:10 -0800

    added more items to the items.py recovery items and finished powders!

--- Lessons/DarkCloud/ModulePackages/ITEMS/items.py
+++ Lessons/DarkCloud/ModulePackages/ITEMS/items.py
@@ -81,8 +81,13 @@ def logger(): # GenesisGir's typical logger preset! 🪵
     
 # logger variable
 logger = logger()
+
+
+
 # import modules
+import sys
 from dataclasses import dataclass, field
+from ModulePackages.PLAYER.Player import *
 
 # lists, tuples, dicts
 CATEGORY_TYPE = ("Powder", 
@@ -93,27 +98,47 @@ CATEGORY_TYPE = ("Powder",
                                 "Power Up Item",
                                     "Crafting Material",)
 
-@dataclass
+@dataclass(frozen=True)
 class Items:
     
     #globals
     global CATEGORY_TYPE
     
     # fields/attrs
-    name: str
-    description: str
-    buy_price: int or float
-    sell_price: int or float
-    item_type: str
+    name: str = field(repr=True) # item name
+    description: str = field(repr=True)
+    hp_restore: int or bool = field(repr=True) # health restore points
+    whp_restore: int = field(repr=True) # weapon restore points
+    buy_price: int or float = field(repr=True)
+    sell_price: int or float = field(repr=True)
+    item_type: str = field(repr=True)
+    dps: int = field(default_factory=0) # damage points
     pass
 
 # object instances
+#powders
 REPAIR_POWDER = Items("Repair Powder","Recovers WHP of weapons.", 20, 10, CATEGORY_TYPE[0])
 AUTO_REPAIR_POWDER = Items("Set as an active item. Auto-repairs weapons about to break..", 200, 100, CATEGORY_TYPE[0])
 ESCAPE_POWDER = Items("Escape Powder","Having this allows an escape from dungeon with command.", 20, 10, CATEGORY_TYPE[0])
 POWERUP_POWDER = Items("Powerup Powder","Unconditionally power up weapon's grade.", None, 10, CATEGORY_TYPE[0])
 STAND_IN_POWDER = Items("Stand-in Powder","Can change to ally when current character's exhausted.", 50, 25, CATEGORY_TYPE[0])
 REVIVAL_POWDER = Items("Revival Powder","Revive from dying conditions.", 100, 50, CATEGORY_TYPE[0])
+
+# recovery 
+BREAD = Items("Bread", "Eating it recovers health a little bit.", 50, None, None, 10, CATEGORY_TYPE[1])
+CHEESE = Items("Cheese", "Eating it allows fairly good recovery of health.", 80, None, None, 30, CATEGORY_TYPE[1])
+MELLOW_BANANA = Items("Mellow Banana", "It recovers HP but make you feel thirsty.", 170, None, None, 45, CATEGORY_TYPE[1])
+PREMIUM_CHICKEN = Items("Premium_Chicken", "Eating it completely recovers health..", 170, None, None, 65, CATEGORY_TYPE[1])
+REGULAR_WATER = Items("Regular Water", "Usual water. Quenches thirst a little bit.", 3, None, None, 5, CATEGORY_TYPE[1])
+TASTY_WATER = Items("Tasty Water", "Water from rugged mountain. Nearly quenches thirst.", 5, None, None, 15, CATEGORY_TYPE[1])
+PREMIUM_WATER = Items("Premium Water", "Amazing water of fairy spring. Completely quenches thirst.", 10, None, None, 30, CATEGORY_TYPE[1])
+ANTIDOTE_DRINK = Items("Antidote Drink", "Drinking it neutralizes the poison in the body.", False, None, None, 40, CATEGORY_TYPE[1]) # cures poison
+SOAP = Items("Soap", "Releases from gooey condition.", False, None, None, 50, CATEGORY_TYPE[1]) # cures gooey condition
+HOLY_WATER = Items("Holy Water", "Releases from spell. Causes major damage to the undead.", False, None, None, 60, CATEGORY_TYPE[1], 1000) # cures curse condition
+MIGHTY_HEALING = Items("Mighty Healing", "Heals all conditions.", False, None, None, 150, CATEGORY_TYPE[1]) # cures all conditions
+STAMINA_DRINK = Items("Stamina Drink", "Drinking it increases power. Ability temporarily increases.", True, None, None, 150, CATEGORY_TYPE[1]) # Grants Pumped Up condition.
+THROBBING_CHERRY= Items("Throbbing Cherry", "Play tag to stop the enemy. Use from menu to cure stop.", False, None, None, 150, CATEGORY_TYPE[1]) # Cures Stop.
+
 
 # item list, tuples, dictionaries
 POWDERS = ( # powder items
@@ -142,27 +167,25 @@ Brooke's Item Shop, Ledan's Item Shop, and the Fairy King's Item Shop. Lana's St
 but not the Auto Repair or Escape variants. And aside from the aforementioned store, Stand-in Powders can also be bought at Ruty's Store.
 """
 
+RECOVERY_ITEMS = ( # recovery items
 
+    #breads & cheeses
+    BREAD,
+    CHEESE,
 
+    # fruits
+    MELLOW_BANANA,
+    PREMIUM_CHICKEN,
+    THROBBING_CHERRY,
+    
+        #drinks
+        REGULAR_WATER,
+        HOLY_WATER,
+        TASTY_WATER,
+        PREMIUM_WATER,
+        ANTIDOTE_DRINK,
+        STAMINA_DRINK,
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    #cleansing
+    SOAP,
+    MIGHTY_HEALING,)
