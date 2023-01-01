7ac1df2310ebbc97c21ee2c05b4f1874f86ab49d
Moore Kitsune <daintygenesis@gmail.com>
2023-01-01 13:43:07 -0800

    item obj instance formating, more features like attachments started

--- Lessons/DarkCloud/ModulePackages/LOCATIONS/dark_cloud_locations.py
+++ Lessons/DarkCloud/ModulePackages/LOCATIONS/dark_cloud_locations.py
@@ -1,3 +1,11 @@
 # location constants
+# villages
 NORUNE_VILLAGE = "Norune Village"
+
+# dungeons
 DIVINE_BEAST_CAVE = "Divine Beast Cave"
+
+# shops
+GAFFERS_BUGGY = "Gaffer's Buggy"
+FRESHEN_UP_WATERY = "Freshen Up Watery"
+FAIRY_KING = "Fairy King"
